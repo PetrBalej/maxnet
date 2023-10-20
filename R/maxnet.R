583195645a09dd0f0a471d175036fa3fc6144bef
Petr Balej <54836928+PetrBalej@users.noreply.github.com>
2023-10-21 00:49:16 +0200

    Update maxnet.R

--- R/maxnet.R
+++ R/maxnet.R
@@ -58,9 +58,6 @@ maxnet <-
 function(p, data, f=maxnet.formula(p, data), regmult=1.0, 
          regfun=maxnet.default.regularization, addsamplestobackground=T, ...)
 {
-   msg <- paste("****************** addsamplestobackground=", as.character(addsamplestobackground), " *********************")
-    print(msg)
-         stop(msg)
    if (anyNA(data)) stop("NA values in data table. Please remove them and rerun.")
    if (!is.vector(p))
        stop("p must be a vector.")
